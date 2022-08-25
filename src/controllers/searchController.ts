6bf91b6cc8df89cac010e14947b5fd25b1f66d14
brunoCesarOliveira <bruno.oliveira@aliare.co>
2022-08-25 07:35:16 -0300

    final projeto 1.0

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,4 +1,19 @@
+import { createMenuObject } from "./../helpers/createMenuObjects"
 import { Request, Response } from "express"
+import { Pet } from "../models/pet"
+
 export const search = (req: Request, res: Response) => {
-  //res.render('pages/search');
+  let query: string = req.query.q as string
+
+  if (!query) {
+    res.redirect("/")
+    return
+  }
+  let list = Pet.getFromName(query)
+
+  res.render("pages/page", {
+    menu: createMenuObject(""),
+    list,
+    query,
+  })
 }
